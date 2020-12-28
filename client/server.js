21639d372becbd2c49cb6f05a34becbad49eebb2
Kevin Masis Leandro <kevinlml@outlook.com>
2020-12-28 14:43:00 -0600

    Add server of webapp

---
+++ client/server.js
@@ -0,0 +1,17 @@
+/*
+Cliente
+Este servidor es el encargado de abrir la webapp
+*/
+
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname + '/source/index.html'));
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
