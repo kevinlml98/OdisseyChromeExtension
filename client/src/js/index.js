c0648eb661935ff5d4aed0efd4cdae066b50237e
Kevin Masis Leandro <kevinlml@outlook.com>
2020-12-31 15:59:42 -0600

    Estructura inicial html cliente
    Se carga un Js provicional

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,1 +1,33 @@
-alert("hola mundo");
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: 'M7lc1UVf-VE',
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// Controles multimedia
+function playVideo() {
+    player.playVideo();
+}
+function pauseVideo() {
+    player.pauseVideo();
+}
