b14013546812f11d1f6279378e5a1ea964127a4d
Andres E. Rojas M <anderojasm@hotmail.com>
2021-01-13 15:03:51 -0600

    Legibilidad del .js y correcion de funciones
    Se eliminaron las funciones viejas y se dejaron las nuevas, las cuales fueron ajustadas para mejor eficacia, al eliminar algunas redundancias

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -5,19 +5,24 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-
 var player;
+var id = 'Xd-luMQNkVw';
+var id2 = 'qZGwRz8wnSY';
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-        height: '260',
-        width: '540',
-        videoId: 'M7lc1UVf-VE',
-        playerVars: { 'autoplay': 1, 'controls': 0 },
+        height: '315',
+        width: '560',
+        videoId: id,
+        
+        playerVars: {'autoplay': 1, 'controls': 0,  'loop': 1},
         events: {
-            'onReady': onPlayerReady
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
         }
     });
 }
+
+document.getElementById('player').style.display='none';
 
 function onPlayerReady(event) {
     let pgd = player.getDuration();
@@ -25,50 +30,71 @@ function onPlayerReady(event) {
     vpb.setAttribute("max",pgd);
 }
 
+//Intento de que la barra de progreso avance con forme al video onPlayerStateChange(event) y onPlay()
+var testThread;
+function onPlayerStateChange(event) {
+    if(event.data == 1)
+    {
+      testThread = setInterval(onPlay,500);
+    }else{
+        clearInterval(testThread);
+    }
+}
+
+function onPlay(){    
+    let vpb = document.getElementById("video_progress_bar");
+    let time = player.getCurrentTime();
+    vpb.setAttribute('value', time); 
+}
+
+
+// Oculta o hace visible el video en la interfaz
+document.getElementById('vis').addEventListener('click',toggle);
+function toggle(element){
+    let ventana = document.getElementById("player");
+    
+    if(ventana.style.display != 'none'){
+        ventana.style.display = 'none';
+    }else{
+        ventana.style.display = '';
+    }
+}
+
+// ----------------------------------------------------------------- //
+//                     Controles multimedia                          //
+// ----------------------------------------------------------------- //
+
+//---------------------------   Play/Pause   ---------------------------
+
 document.getElementById('btnplay').addEventListener('click',playV);
 function playV(element){
     let btn = document.getElementById("btnplay");
-    btn.style.display = 'none';
-    let btn2 = document.getElementById("btnpause");
-    btn2.style.display = '';
-    player.playVideo();
-}
-document.getElementById('btnpause').addEventListener('click',pauseV);
-function pauseV(element){
-    let btn = document.getElementById("btnplay");
-    btn.style.display = '';
-    let btn2 = document.getElementById("btnpause");
-    btn2.style.display = 'none';
-    player.pauseVideo();
-}
-/*
-// Controles multimedia
-function playVideo() {
-    let btn = document.getElementById("btnplay");
-    btn.setAttribute("onclick","pauseVideo()");
-    btn.setAttribute("value","pause");
-    player.playVideo();
-}
-function pauseVideo() {
-    let btn = document.getElementById("btnplay");
-    btn.setAttribute("onclick","playVideo()");
-    btn.setAttribute("value","play");
-    player.pauseVideo();
+    
+    if(btn.value === 'play'){
+        btn.setAttribute("value","pause");
+        player.playVideo();
+    }
+    else{
+        btn.setAttribute("value","play");
+        player.pauseVideo();
+    }
 }
 
-// Volumen__________________________________________________________________________
+//---------------------------   Volumen   ---------------------------
+
 const volumen = document.getElementById('volumen')
 
 volumen.addEventListener('input', function() {
-  let v = volumen.value;
-  player.setVolume(v);
+    let v = volumen.value;
+    player.setVolume(v);
 });
 
-// ProgressBar__________________________________________________________________________
+//---------------------------   ProgressBar   ---------------------------
+
 const vpb = document.getElementById('video_progress_bar')
 
 vpb.addEventListener('input', function() {
-  let rb = vpb.value;
-  player.seekTo(rb);
-
-});*/
+    let rb = vpb.value;
+    player.seekTo(rb);
+    
+});
