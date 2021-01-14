2ebf4f6f14f51ddbaf29da74a38cd5769f415231
Kevin Masis Leandro <kevinlml@outlook.com>
2021-01-14 00:43:23 -0600

    Fix API
    Se añade autentificacion por medio de header

--- api-rest/dbconection.js
+++ api-rest/dbconection.js
@@ -20,12 +20,3 @@ mysqlConnection.connect(function (err) {
 
 module.exports = mysqlConnection;
 
-/*
-const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'odisseydb',
-  multipleStatements: true
-});
-*/
