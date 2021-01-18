22af845218cad5c13532edf6ca4e12105aff4b42
Andres E. Rojas M <anderojasm@hotmail.com>
2021-01-18 01:05:25 -0600

    Requests al API y Funcion de registro de usuario
    Creacion de funciones fetch para comunicar el cliente y el API.
    Funcion de Autentificacion y registro forzado

--- client/src/js/apiconnect.js
+++ client/src/js/apiconnect.js
@@ -1,62 +1,39 @@
-// OMNIBOX _________________________________________________________
+//Variables para identificacion
+var userId = 1;
+var userCorreo;
+const globalUrl = `http://localhost:4000`;
+var canUseApp = false;
 
-
-
-chrome.omnibox.onInputChanged.addListener(
-
-
-    function(text, suggest)
-    {
-        suggest([
-            { content: "primera opcion", description: "1er opcion " + text },
-            { content: "segunda opcion" , description: "2da opcion " + text }
-        ]);
-    }
-
-
-
-
-);
-
-
-chrome.omnibox.onInputEntered.addListener(
-
-
-
-);
-
+chrome.identity.getProfileUserInfo(function(userInfo) {
+  console.log(userInfo);
+  userCorreo = userInfo.email;
+})
 
 // FETCH --> Retorna una promesa
 
-//var req = 'users';
-var id = 1;
-//var url = `http://localhost:4000/${req}`;
-var nombre;
-var correo;
-
-
 // GET/users___________________________________
 function GET_AllUsers(){
-    var url = `http://localhost:4000/users`;
-    fetch( url, {
+    var path = `/users`;
+    fetch( globalUrl + path, {
         method: 'GET',
         headers: {
-            'usuario' : id
+            'usuario' : userId
           }
     })
     .then(response => response.json())
     .then(data => console.log(data)).catch( error => {
       console.log(error);
-    });   
+    });
 }
 
-// GET/songs___________________________________
+// GET/songs/id___________________________________
 function GET_Soundtracks(req){
-    var url = `http://localhost:4000/songs/${req}`;
-    fetch( url, {
+    var path = `/songs/${req}`;
+    fetch( globalUrl + path, {
         method: 'GET',
         headers: {
-            'usuario' : id
+          'content-type': 'application/json',
+            'usuario' : userId
           }
     })
     .then(response => response.json())
@@ -64,32 +41,167 @@ function GET_Soundtracks(req){
         console.log(data);
         console.log(data.length);
 
-    
+
     }).catch( error => {
       console.log(error);
-    });   
-}
-
-
-
-
-
-
-// POST ___________________________________
-async function PostData(){
-    fetch( url, {
-        method: 'POST',
-        header :{
-            'usuario' : usuario
-        },
-        body: JSON.stringify({
-            name : nombre,
-            email : correo
-        })
-    }).then( (response) => response.json()).then(data => {
-        
     });
 }
 
+// GET/songs___________________________________
+function GET_AllSoundtracks(){
+    var path = `/songs`;
+    fetch( globalUrl + path, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+            'usuario' : userId
+          }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.length);
 
 
+    }).catch( error => {
+      console.log(error);
+    });
+}
+
+// POST/users ___________________________________
+function PostData(pEmail){
+  var path = `/users`;
+    fetch(globalUrl + path, {
+      method : 'POST',
+      headers : {
+        'content-type': 'application/json',
+      },
+      body : JSON.stringify({email:pEmail})
+      })
+      .then( (response) => response.json())
+      .then(data => {
+        console.log(data.status);
+      });
+}
+
+// GET/checkuser ___________________________________
+/*
+async function CheckUser(pEmail){
+  var url = `http://localhost:4000/checkuser/${pEmail}`;
+  let res = await fetch( url, {
+      headers:{
+        'content-type': 'application/json',
+    }
+    })
+    .then( (response) => response.json())
+    .then(data => {
+      //console.log(data);
+      return data;
+    });
+    return res;
+}
+*/
+async function CheckUser(pEmail){
+  var path = `/checkuser/${pEmail}`;
+  const respuesta = await fetch(globalUrl + path);
+  const data = await respuesta.json();
+  console.log(data);
+  return data;
+}
+// GET/users/id___________________________________
+async function GetUserID(pId){
+  var path = `/users/${pId}`;
+  const respuesta = await fetch(globalUrl + path,
+  {
+    headers:{
+      'content-type': 'application/json',
+      usuario : userId
+  }
+  });
+  const data = await respuesta.json();
+  console.log(data);
+  return data;
+}
+
+// DELETE /users/id___________________________________
+function DeleteUsers(pId){
+  var path = `/users/${pId}`;
+    fetch(globalUrl+path, {
+      method : 'DELETE',
+      headers : {
+        'content-type': 'application/json',
+        usuario : userId
+      }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.status);
+      });
+}
+// DELETE/songs/id___________________________________
+function DeleteSong(pSongId){
+  var path = `/songs/${pSongId}`;
+    fetch(globalUrl+path, {
+      method : 'DELETE',
+      headers : {
+        'content-type': 'application/json',
+        usuario : userId
+      }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.status);
+      });
+}
+
+//Autorizacion y registro
+async function Auth(pEmail)
+{
+  if (userCorreo != "")
+  {
+    var isThere = await CheckUser(userCorreo);
+    if (  isThere.exist != true)
+    {
+      PostData(userCorreo);
+      isThere =  await CheckUser(userCorreo);
+      userId = isThere.body[0].US_Id;
+      console.log(userId);
+    }
+    else
+      {
+        userId = isThere.body[0].US_Id;
+        console.log(userId);
+      }
+      canUseApp = true;
+  }
+  else{
+    console.log("Usuario no esta registrado en chrome");
+  }
+}
+
+// OMNIBOX _________________________________________________________
+chrome.omnibox.onInputChanged.addListener(function(text, suggest)
+    {
+      if (canUseApp)
+      {
+        suggest([
+            { content: "primera opcion", description: "1er opcion " + text },
+            { content: "segunda opcion" , description: "2da opcion " + text }
+        ]);
+      }
+      else {
+        suggest([
+            { content: "Sincronice su cuenta en chrome para usar la aplicacion", description: "Sincronice su cuenta en chrome para usar la aplicacion" + text },
+        ]);
+      }
+    }
+);
+
+
+chrome.omnibox.onInputEntered.addListener(function(input){
+
+  if (!canUseApp)
+  {
+      console.log("loggeese");
+  }
+});
