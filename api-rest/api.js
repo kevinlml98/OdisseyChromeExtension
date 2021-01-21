ed4af875bd163b46b15c404fab58b3d53100737c
Andres E. Rojas M <anderojasm@hotmail.com>
2021-01-21 00:34:42 -0600

    Docu HTML
    documentacion html generada por medio de JSdoc

--- api-rest/api.js
+++ api-rest/api.js
@@ -5,6 +5,10 @@ const mysqlConnection = require('./dbconection');
 
 
 //   GET /checkuser/____________________________________
+/**
+ * Evalua si un usuario existe en la base dde datos por medio del email registrado.
+ * @returns {object} JSON Format, Bool si lo encuentra o no, de ser positivo retorna los datos relacionados
+ */
 router.get('/checkuser/:email', (req, res) => {
 
   const email = req.params['email'];
@@ -31,6 +35,10 @@ router.get('/checkuser/:email', (req, res) => {
 
 
 //   GET /users____________________________________________
+/**
+ * Obtiene la lista de usuarios. Debe estar registrado en la base de datos
+ * @return {Object} JSON Format, envia una lista con los resultados registrados en la base.
+ */
 router.get('/users', (req, res) => {
 
   const header = req.header('usuario');
@@ -66,6 +74,10 @@ router.get('/users', (req, res) => {
 });
 
 //   GET /users?id=parametro_______________________________
+/**
+ * Consulta la lista de usuarios buscando alguno cuyo ID haga match con el parámetro. Debe estar registrado en la base de datos
+ * @return {Object} JSON Format, confirmacion de si existe o no, de encontrarlo devuelve los datos relacionados al usuario
+ */
 router.get('/users/:id', (req, res) => {
 
 
@@ -103,6 +115,10 @@ router.get('/users/:id', (req, res) => {
 
 
 //   GET /users/exists?id=parametro________________________
+/**
+ * Evalua si un usuario existe o no en la base de datos. Debe estar registrado en la base de datos
+ * @return {Object} JSON Format, envia una confirmacion, si lo encientra devuelve los datos
+ */
 router.get('/users/exists/:id', (req, res) => {
   const { id } = req.params;
   mysqlConnection.query(`call ReturnUser(${id})`, (error, result) => {
@@ -127,6 +143,10 @@ router.get('/users/exists/:id', (req, res) => {
 
 
 //   POST /users___________________________________________
+/**
+ * Registra un nuevo usuario en la base de datos.
+ * @return {Object} JSON Format, confirmacion de la accion
+ */
 router.post('/users', (req, res) => {
   const sql = `call AddUser(?)`;
   const userBody = {
@@ -143,6 +163,10 @@ router.post('/users', (req, res) => {
 
 
 //   DELETE /users/id______________________________________
+/**
+ * Elimina el usuario indicado por id de la base de datos. Requiere de un rol administrador
+ * @return {Object} JSON Format, Confirmacion de la peticion
+ */
 router.delete('/users/:id', (req, res) => {
 
   const header = req.header('usuario');
@@ -179,6 +203,10 @@ router.delete('/users/:id', (req, res) => {
 
 
 //   GET /songs____________________________________________
+/**
+ * Obtiene la lista de todas las canciones. El usuario debe estar registrado en la base de datos
+ * @return {Object} JSON Format una lista con todas las canciones
+ */
 router.get('/songs', (req, res) => {
 
   const header = req.header('usuario');
@@ -218,6 +246,10 @@ router.get('/songs', (req, res) => {
 });
 
 //   DELETE /songs/id______________________________________
+/**
+ * Elimina la canción indicada por el id. Requiere de un rol de administrador.
+ * @return {Object} JSON Format, un mensaje que confirma o niega la peticion
+ */
 router.delete('/songs/:id', (req, res) => {
 
   const header = req.header('usuario');
@@ -254,6 +286,10 @@ router.delete('/songs/:id', (req, res) => {
 
 
 //   GET /songs?search=____________________________________
+/**
+ * Busca las canciones según una consulta del usuario. Debe estar registrado en la base de datos
+ * @return {Object} JSON Format, si lo encuentra envia una lista con los resultados
+ */
 router.get('/songs/:search', (req, res) => {
 
   const header = req.header('usuario');
